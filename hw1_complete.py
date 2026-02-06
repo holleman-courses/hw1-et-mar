f3e9126c6e5659a1ac280ce142e6b14775d5b921
ethan <emarti77@charlotte.edu>
2026-02-06 11:16:19 -0500

    Built Model 1, tests passed

---
+++ hw1_complete.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+import keras
+from keras import Input, layers, Sequential
+
+# Helper libraries
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import image
+
+
+print(f"TensorFlow Version: {tf.__version__}")
+print(f"Keras Version: {keras.__version__}")
+
+# Test comment
+## 
+
+def build_model1():
+  model = tf.keras.Sequential([
+    Input(shape=(32, 32, 3)),
+    layers.Flatten(),
+    layers.Dense(128, activation='leaky_relu'),
+    layers.Dense(128, activation='leaky_relu'),
+    layers.Dense(128, activation='leaky_relu'),
+    layers.Dense(10, activation=None)
+  ])
+  return model
+
+def build_model2():
+  model = None # Add code to define model 1.
+  return model
+
+def build_model3():
+  model = None # Add code to define model 1.
+  ## This one should use the functional API so you can create the residual connections
+  return model
+
+def build_model50k():
+  model = None # Add code to define model 1.
+  return model
+
+# no training or dataset construction should happen above this line
+# also, be careful not to unindent below here, or the code be executed on import
+if __name__ == '__main__':
+
+  ########################################
+  ## Add code here to Load the CIFAR10 data set
+
+  ########################################
+  ## Build and train model 1
+  model1 = build_model1()
+  # compile and train model 1.
+  model1.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+  print("Model 1 Summary:")
+  print(model1.summary())
+  ## Build, compile, and train model 2 (DS Convolutions)
+  model2 = build_model2()
+
+  
+  ### Repeat for model 3 and your best sub-50k params model
+  
+  
